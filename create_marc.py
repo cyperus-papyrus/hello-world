8f821e54b2d8b79c711d5d34e2abb7cc396b3638
Eugene Gavrish <eug033@gmail.com>
2016-03-10 08:59:28 +0300

    в виде пригодного модуля

--- create_marc.py
+++ create_marc.py
@@ -54,55 +54,59 @@ def field2string(f):
 def record2text(r):
     return u'\n'.join([field2string(field) for field in sorted(r.fields, key=lambda y: y.tag)])
 
+def do_create_marc():
+    engine = create_engine('mysql://marc:123@localhost/marc?charset=utf8',
+                           encoding='utf-8', convert_unicode=True, echo=True)  # подключение к БД
+    connection = engine.connect()
+    connection.execute("SET character_set_connection=utf8")
+    result = connection.execute(
+        text("SELECT id, field, info_text FROM marc.aleph2 WHERE id LIKE '%Ru-MoLR' ORDER BY ID, FIELD"))
+    r = pymarc.Record(to_unicode=True, force_utf8=True)
+    mypath = os.path.dirname(os.path.abspath(__file__))
+    writer = pymarc.MARCWriter(open('%s/static/marc_cards.mrc' % mypath, 'wb'))
+    text_writer = io.open('%s/static/marc_cards.txt' % mypath, 'w', encoding='utf-8')
+    test = io.open('%s/static/bad_marc_cards.txt' % mypath, 'w', encoding='utf-8')
+    (id, field, info) = result.fetchone()
+    current_num = id
+    process_field(field, info, r)
+    count = 0
+    counter = 0
 
-engine = create_engine('mysql://marc:123@localhost/marc?charset=utf8',
-                       encoding='utf-8', convert_unicode=True, echo=True)  # подключение к БД
-connection = engine.connect()
-connection.execute("SET character_set_connection=utf8")
-result = connection.execute(
-    text("SELECT id, field, info_text FROM marc.aleph2 WHERE id LIKE '%Ru-MoLR' ORDER BY ID, FIELD"))
-r = pymarc.Record(to_unicode=True, force_utf8=True)
-mypath = os.path.dirname(os.path.abspath(__file__))
-writer = pymarc.MARCWriter(open('%s/static/marc_cards.mrc' % mypath, 'wb'))
-text_writer = io.open('%s/static/marc_cards.txt' % mypath, 'w', encoding='utf-8')
-test = io.open('%s/static/bad_marc_cards.txt' % mypath, 'w', encoding='utf-8')
-(id, field, info) = result.fetchone()
-current_num = id
-process_field(field, info, r)
-count = 0
-counter = 0
+    for (id, field, info) in result.fetchall():
+        if id == current_num:
+            process_field(field, info, r)
+        else:
+            current_num = id
+            test_string = StringIO.StringIO()
+            testwriter = pymarc.MARCWriter(test_string)
+            testwriter.write(r)
+            test1 = pymarc.MARCReader(test_string.getvalue(), to_unicode=True, force_utf8=True)
+            try:
+                for x in test1:
+                    count += 1
+            except UnicodeDecodeError:
+                counter += 1
+                test.write(record2text(r) + u'\n\n\n')
+            testwriter.close()
+            writer.write(r)
+            text_writer.write(record2text(r) + u'\n\n\n')
+            r = pymarc.Record(to_unicode=True, force_utf8=True)
+            process_field(field, info, r)
 
-for (id, field, info) in result.fetchall():
-    if id == current_num:
-        process_field(field, info, r)
     else:
-        current_num = id
-        test_string = StringIO.StringIO()
-        testwriter = pymarc.MARCWriter(test_string)
-        testwriter.write(r)
-        test1 = pymarc.MARCReader(test_string.getvalue(), to_unicode=True, force_utf8=True)
-        try:
-            for x in test1:
-                count += 1
-        except UnicodeDecodeError:
-            counter += 1
-            test.write(record2text(r) + u'\n\n\n')
-        testwriter.close()
         writer.write(r)
-        text_writer.write(record2text(r) + u'\n\n\n')
-        r = pymarc.Record(to_unicode=True, force_utf8=True)
-        process_field(field, info, r)
-
-else:
-    writer.write(r)
-    count += 1
-    text_writer.write(record2text(r))
-writer.close()
-text_writer.close()
-test.close()
-print count
-print "Bad records counter:", counter
-total = int(count) + int(counter)
-num_file = open('num_file.txt', 'w')
-num_file.write(str(total) + u'\n' + str(count) + u'\n' + str(counter))
-num_file.close()
+        count += 1
+        text_writer.write(record2text(r))
+    writer.close()
+    text_writer.close()
+    test.close()
+    print count
+    print "Bad records counter:", counter
+    total = int(count) + int(counter)
+    num_file = open('num_file.txt', 'w')
+    num_file.write(str(total) + u'\n' + str(count) + u'\n' + str(counter))
+    num_file.close()
+    
+if __name__ == "__main__":
+    do_create_marc()
+    
