4a839b6ae02b7c38a11aa3ecea554fc4bc8efe82
Eugene Gavrish <eug033@gmail.com>
2016-03-15 09:41:37 +0300

    удалим pid

--- run.sh
+++ run.sh
@@ -17,6 +17,6 @@ echo "MYPID=$$" > $PID 2>/dev/null
 cd $HOMEDIR
 /home/helga/olgavr/venv/bin/python2.7 $HOMEDIR/$PROGNAME.py > $PROGNAME.log 2>&1
 sleep 1
-
+rm -f $PID 2>/dev/null
 ################################################################################
 exit 0
