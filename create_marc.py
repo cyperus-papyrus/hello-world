2a2e7a259a9f6b8ece17338482df07834c7a018a
cyperus_papyrus <cdg013@gmail.com>
2016-03-21 12:28:07 +0300

    патч для 32 последних карточек

--- create_marc.py
+++ create_marc.py
@@ -75,7 +75,8 @@ def do_create_marc():
         print number
         number = number + u'Ru-MoLR'
         nums.append(number)
-    nums1 = ', '.join(nums)
+    nums1 = '\', \''.join(nums)
+    nums1 = '\'' + nums1 + '\''
     result = connection.execute("SELECT id, field, info_text FROM marc.aleph2 WHERE id IN('%s') ORDER BY ID, FIELD;" % nums1)
     r = pymarc.Record(to_unicode=True, force_utf8=True)
     (id, field, info) = result.fetchone()
